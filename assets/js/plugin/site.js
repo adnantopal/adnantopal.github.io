bbf4a712a870ebd9bab6b56f4ecb6af053f1cd2c
Adnan Topal <adnan@adnan.co>
2016-04-11 18:37:37 +0300

    continued.

--- assets/js/plugin/site.js
+++ assets/js/plugin/site.js
@@ -2,25 +2,6 @@
     "use strict";
     
     $(function () {
-        var texts = [];
-
-        $('.PostBody').find('h2, h3').waypoint(function(direction) {
-            var $current = $(this.element);
-
-            if ("down" === direction) {
-                $('.ActiveSection').fadeOut(function() {
-                    texts.push($(".ActiveSection").text());
-                    $(this).text($current.text()).fadeIn();
-                });
-            } else if ("up" === direction) {
-                $('.ActiveSection').fadeOut(function() {
-                    $(this).text(texts.pop()).fadeIn();
-                });
-            }
-        }, {
-            offset: '35%'
-        });
-        
         $(document.body).on('click', '.NavToggle', function(e) {
             e.preventDefault();
             
